fdc3c3cb660d89fd3f715510229cefa5cd18caf1
Gary van der Merwe <gary@lsd.co.za>
2017-07-05 14:42:51 +0200

    Don't show exeptions for mapmytracks aiohttp client errors.

--- trackers/map_my_tracks.py
+++ trackers/map_my_tracks.py
@@ -240,6 +240,8 @@ async def monitor_user(client_session, user, start_date, end_date, cache_path, t
             await asyncio.sleep(30)
         except asyncio.CancelledError:
             break
+        except aiohttp.client_exceptions.ClientError as e:
+            tracker.logger.exception('Error in monitor_user: {}'.format(e))
         except Exception:
             tracker.logger.exception('Error in monitor_user:')
             await asyncio.sleep(10)
