89560596889f9c132e1ee42790c6149acab73ff8
Gary van der Merwe <gary.vandermerwe@fnb.co.za>
2023-04-09 21:35:38 +0200

    Fix geodisic tests.

--- trackers/tests/test_geodisic_helpers.py
+++ trackers/tests/test_geodisic_helpers.py
@@ -11,72 +11,68 @@ from trackers.analyse import (
 )
 
 
-class TestFindCPoint(unittest.TestCase):
-    def test_between(self):
-        result = find_c_point(Point(0.0001, 15), Point(0, 0), Point(0, 30))
-        self.assertEqual(result.dist, 11.057427582158148)
-        self.assertEqual(result.point, Point(lat=0.0, lng=14.999999999999998))  # Rounding wtf?
-
-    def test_outside(self):
-        result = find_c_point(Point(0.0001, 40), Point(0, 0), Point(0, 30))
-        self.assertEqual(result.point, Point(lat=0.0, lng=30))
-
-    def test_same(self):
-        result = find_c_point(Point(0, 0), Point(0, 0), Point(0, 30))
-        self.assertEqual(result.point, Point(lat=0, lng=0))
+def round_point(point: Point, digits):
+    return Point(lat=round(point.lat, digits), lng=round(point.lng, digits))
 
 
-class TestRamerDouglasPeucker(unittest.TestCase):
-    def test_ramer_douglas_peucker(self):
-        points = [
-            Point(0, 0),
-            Point(0.0001, 15),
-            Point(0, 30),
-            Point(15, 45),
-            Point(0, 60),
-        ]
-
-        simple_points = ramer_douglas_peucker(points, 20)
-        self.assertEqual(
-            simple_points,
-            (
-                Point(0, 0),
-                Point(0, 30),
-                Point(15, 45),
-                Point(0, 60),
-            ),
-        )
+def test_find_c_point_between():
+    result = find_c_point(Point(0.0001, 15), Point(0, 0), Point(0, 30))
+    assert round(result.dist, 4) == 11.0574
+    assert round_point(result.point, 4) == Point(lat=0.0, lng=15.0)
 
 
-class TestDistance(unittest.TestCase):
-    def test_distance(self):
-        dist = distance(Point(0, 0), Point(0.0001, 0))
-        self.assertEqual(dist, 11.057427582158146)
+def test_find_c_point_outside():
+    result = find_c_point(Point(0.0001, 40), Point(0, 0), Point(0, 30))
+    assert result.point == Point(lat=0.0, lng=30)
 
 
-class TestGetEqualSpacedPoints(unittest.TestCase):
-    def test(self):
-        points = list(
-            get_equal_spaced_points(
-                [
-                    Point(0, 0),
-                    Point(0, 0.001),
-                    Point(0.001, 0.001),
-                ],
-                50,
-            )
-        )
-        self.assertEqual(
-            points,
+def test_find_c_point_same():
+    result = find_c_point(Point(0, 0), Point(0, 0), Point(0, 30))
+    result.point == Point(lat=0, lng=0)
+
+
+def test_ramer_douglas_peucker():
+    points = [
+        Point(0, 0),
+        Point(0.0001, 15),
+        Point(0, 30),
+        Point(15, 45),
+        Point(0, 60),
+    ]
+
+    simple_points = ramer_douglas_peucker(points, 20)
+    assert simple_points == (
+        Point(0, 0),
+        Point(0, 30),
+        Point(15, 45),
+        Point(0, 60),
+    )
+
+
+def test_distance():
+    dist = distance(Point(0, 0), Point(0.0001, 0))
+    assert round(dist, 4) == 11.0574
+
+
+def test_get_equal_spaced_points():
+    points = list(
+        get_equal_spaced_points(
             [
-                (Point(lat=0, lng=0), 0),
-                (Point(lat=0.0, lng=0.000449), 50),
-                (Point(lat=0.0, lng=0.000898), 100),
-                (Point(lat=0.000350, lng=0.001), 150),
-                (Point(lat=0.000802, lng=0.001), 200),
-                (Point(lat=0.001, lng=0.001), 221.89376661216434),
+                Point(0, 0),
+                Point(0, 0.001),
+                Point(0.001, 0.001),
             ],
+            50,
         )
+    )
+    assert [(point, round(cum_dist, 4)) for point, cum_dist in points] == [
+        (Point(lat=0, lng=0), 0),
+        (Point(lat=0.0, lng=0.000449), 50),
+        (Point(lat=0.0, lng=0.000898), 100),
+        (Point(lat=0.000350, lng=0.001), 150),
+        (Point(lat=0.000802, lng=0.001), 200),
+        (Point(lat=0.001, lng=0.001), 221.8938),
+    ]
 
 
 class TestMoveAlongRoute(unittest.TestCase):
